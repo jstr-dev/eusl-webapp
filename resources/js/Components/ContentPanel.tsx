3775ecf647d8e025e7cf3bfdea9c986136df9491
jstr-dev <adamcgl@outlook.com>
2024-06-21 22:53:15 +0100

    fix

--- resources/js/Components/ContentPanel.tsx
+++ resources/js/Components/ContentPanel.tsx
@@ -3,7 +3,7 @@ import {PropsWithChildren} from "react";
 export default function ContentPanel({ children, title }: PropsWithChildren<{ title: string | null }>) {
     return (<>
         <div className='content rounded-lg'>
-            {title !== null &&
+            {title &&
                 <div className={'flex flex-col w-full gap-2'}>
                     <h1 className='text-lg mb-2 font-semibold' style={{ marginTop: '-7px' }}>{title}</h1>
                     <hr className='mb-4 ignore-content-padding' />
